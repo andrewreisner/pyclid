4dfa6599f10e6f34e8bdfd8e68a23d19e4eba08d
Andrew Reisner <areisne2@illinois.edu>
2015-11-27 23:36:07 -0600

    Simple QR example working.

--- pyclid/qr.py
+++ pyclid/qr.py
@@ -15,29 +15,48 @@ def iddr_qrpiv(queue, m, n, a, krank, ind, ss):
     qr_prg = cl.Program(ctx, util.get_source('qr_kerns.cl')).build()
 
     # begin debug
-    # m = 4
-    # n = 3
-    # adebug = np.zeros((m,n), dtype=a.dtype)
-    # adebug[:,0] = np.linspace(0,1,m)
-    # adebug[:,1] = np.linspace(1,2,m)
-    # adebug[:,2] = np.linspace(2,3,m)
+    m = 6
+    n = 3
+    adebug = np.zeros((m,n), dtype=a.dtype)
+    adebug[:,0] = np.array([1,0,0,-1,-1,0])
+    adebug[:,1] = np.array([0,1,0,1,0,-1])
+    adebug[:,2] = np.array([0,0,1,0,1,1])
 
-    # cla = cl_array.Array(queue, adebug.shape, adebug.dtype)
-    # cla.set(adebug)
-    # ssdebug = cl_array.Array(queue, n, dtype=ss.dtype)
+    cla = cl_array.Array(queue, adebug.shape, adebug.dtype)
+    cla.set(adebug)
+    ssdebug = cl_array.Array(queue, n, dtype=ss.dtype)
+    rdebug = cl_array.Array(queue, cla.shape, cla.dtype)
+    rdebug.set(np.zeros(cla.shape))
     # for i in range(n):
     #     print(np.sum(adebug[:,i]**2))
     # end debug
 
     ss_l = cl.LocalMemory(m*np.dtype('float64').itemsize)
-    qr_prg.ss(queue, [m,n], [m,1], a.data, ss.data, ss_l)
-    kpiv = util.argmax(queue, ss)
+    # qr_prg.ss(queue, [m,n], [m,1], a.data, ss.data, ss_l)
+    # kpiv = util.argmax(queue, ss)
+    qr_prg.ss(queue, [m,n], [m,1], cla.data, ssdebug.data, ss_l)
+
+    qk = cl.LocalMemory(m*np.dtype('float64').itemsize)
+    aj_qk = cl.LocalMemory(m*np.dtype('float64').itemsize)
 
     nloops = np.min([m,n,krank])
-    for k in range(nloops):
-        ind[k] = kpiv
-        qr_prg.swap_col(queue, [m, 1], [m, 1], a.data,
-                        np.int32(k), np.int32(kpiv), np.int32(n))
-        ss[k], ss[kpiv] = ss[kpiv], ss[k]
-        qr_prg.proj_rm(queue, [m, n-k], [m, 1], a.data,
-                       ss_l)
+    for k in range(nloops-1):
+        # ind[k] = kpiv
+        # qr_prg.swap_col(queue, [m, 1], [m, 1], a.data,
+        #                 np.int32(k), np.int32(kpiv), np.int32(n))
+        # ss[k], ss[kpiv] = ss[kpiv], ss[k]
+        # qr_prg.proj_rm(queue, [m, n-k], [m, 1],
+        #                a.data, r.data, ss.data,
+        #                qk, ss_l, aj_qk, k, n)
+        qr_prg.proj_rm(queue, [m, n-(k+1)], [m, 1],
+                       cla.data, rdebug.data, ssdebug.data,
+                       qk, ss_l, aj_qk, np.int32(k), np.int32(n))
+        #print(cla.get())
+        qr_prg.ss(queue, [m,n], [m,1], cla.data, ssdebug.data, ss_l)
+        #print(rdebug.get())
+        #print(ssdebug.get())
+    qr_prg.norm(queue, [m,1], [m, 1],
+                rdebug.data, ssdebug.data,
+                np.int32(nloops-1), np.int32(n))
+    print(rdebug.get())
+    #print(rdebug.get())
--- pyclid/qr_kerns.cl
+++ pyclid/qr_kerns.cl
@@ -3,9 +3,19 @@ inline void reduce_group_sum(__local double *ss_l)
 	int lid = get_local_id(0);
 	int group_size = get_local_size(0);
 
+	// Naively round down to nearest power of 2
+	int pwt = 1;
+	while (pwt <= group_size) pwt <<= 1;
+	pwt >>= 1;
+
+	barrier(CLK_LOCAL_MEM_FENCE);
+	// preprocess: sum values beyond pwt into beginning of array
+	if ((lid+1) > pwt) {
+		ss_l[(lid+1)^pwt] += ss_l[lid];
+	}
 	barrier(CLK_LOCAL_MEM_FENCE);
 
-	for (int i = group_size/2; i > 0; i >>= 1) {
+	for (int i = pwt/2; i > 0; i >>= 1) {
 		if (lid < i) {
 			ss_l[lid] += ss_l[lid + i];
 		}
@@ -43,10 +53,42 @@ __kernel void swap_col(__global double *a, int c1, int c2, int stride)
 }
 
 
-__kernel void proj_rm(__global double *a_g, __local double *qk, int k, int stride)
+__kernel void proj_rm(__global double *a_g, __global double *r_g,
+                      __global double *ss,
+                      __local double *qk, __local double *a_l,
+                      __local double *aj_qk, int k, int stride)
 {
 	int lid = get_local_id(0);
+	int gid = get_group_id(1);
 	int ki = k + lid*stride;
+	int jdx = (k + gid + 1) + lid*stride;
 
+
+	double rkk = sqrt(ss[k]);
+	if (get_global_id(0) == 0 && get_global_id(1) == 0) {
+		r_g[k + k*stride] = rkk;
+	}
+	qk[lid] = a_g[ki] / rkk;
+	a_l[lid] = a_g[jdx];
+	aj_qk[lid] = qk[lid]*a_l[lid];
+
+	reduce_group_sum(aj_qk);
+
+	double r_kj = aj_qk[0];
+	if (lid == 0) {
+		r_g[k*stride + (k + gid + 1)] = r_kj;
+	}
+
+	a_g[jdx] = a_l[lid] - r_kj*qk[lid];
 }
 
+
+__kernel void norm(__global double *r_g, __global double *ss,
+                   int k, int stride)
+{
+	/* Doesn't do much now, anticipating the potential need
+	   for Q */
+	if (get_global_id(0) == 0 && get_global_id(1) == 0) {
+		r_g[k+k*stride] = sqrt(ss[k]);
+	}
+}
